698a01466b4426739c6438d27b4d6acbd36c74aa
Matt Wins <mattwins@Matt-Winss-Mac-mini.local>
2011-02-27 19:06:25 +0000

    Updated thumbnail size

--- app/models/gallery_image.rb
+++ app/models/gallery_image.rb
@@ -4,9 +4,9 @@ class GalleryImage < ActiveRecord::Base
   validates_presence_of :title, :description, :visible
   validates_uniqueness_of :title
   
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :photo, :styles => { :medium => "800x800>", :thumb => "240x125#" }
   
   def self.find_visible
     find_all_by_visible(true, :order => :title)
   end
-end
+end  
